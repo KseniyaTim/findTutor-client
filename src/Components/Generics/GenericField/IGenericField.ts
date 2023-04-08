413aff8368cf90bab9040b0f745113f1c7cbe17a
ShaharKrichli <58039507+ShaharKrichli@users.noreply.github.com>
2023-04-08 22:42:17 +0300

    creating the functions validators

--- src/Components/Generics/GenericField/IGenericField.ts
+++ src/Components/Generics/GenericField/IGenericField.ts
@@ -1,4 +1,21 @@
-export interface IGenericField{
+export enum FIELDS_VALIDATORS_FUNCTIONS {
+    checkLength = 'checkLength',
+    maxLength = 'maxLength',
+    minLength = 'minLength',
+    onlyDigits = 'onlyDigits',
+    onlyHebrewLetters = 'onlyHebrewLetters',
+    checkEmail = 'checkEmail',
+    required = 'required',
+    phoneNumber = 'phoneNumber',
+}
+
+export type FIELDS_VALIDATORS_FUNCTIONS_LITERALS = keyof typeof FIELDS_VALIDATORS_FUNCTIONS;
+
+export type fieldsValidatorsManager = { [literal in FIELDS_VALIDATORS_FUNCTIONS_LITERALS]:
+    (...args: any[]) => boolean };
+
+
+export interface IGenericField {
     label: string
-    validationFuncs?: string[]
+    validationFuncs?: FIELDS_VALIDATORS_FUNCTIONS_LITERALS[]
 }
